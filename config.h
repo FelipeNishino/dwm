4f0c47744a4e9e25bd2cac64c9e693ca3aa1628f
FelipeNishino <felipenishino@gmail.com>
2022-03-17 00:01:46 -0300

    Patch: shiftview

--- config.h
+++ config.h
@@ -25,6 +25,8 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+#include "shiftview.c"
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -102,6 +104,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_n,      shiftview,      {.i = +1} },
+	{ MODKEY|ShiftMask,             XK_n,      shiftview,      {.i = -1} },	
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
